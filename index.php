bd1a71aefc0e8b5e904fe7370b0aac1e4abbf29a
maksimsivanovs <maksims.ivanovs@redboxdigital.com>
2017-11-27 22:27:55 +0200

    Repalced logo Added form

--- index.php
+++ index.php
@@ -11,31 +11,28 @@
     <link rel="stylesheet" href="stylesheets/main.css">
 </head>
 <body>
-<div class="logo">
-    <h1>
-        <img src="images/ios-logo.png" alt="iOS">
-        <span>iOS iCloud Unlock</span>
-    </h1>
-</div>
-<div class="device-background">
-    <div class="device-text">
-        <h4>Enter your details to unlock</h4>
-        <input type="text" placeholder="Enter your ECID">
-        <input type="text" placeholder="Enter your serial number">
-        <select name="ios" id="ios">
-            <option value="1033">10.3.3</option>
-            <option value="1100">11.0.0</option>
-            <option value="1103">11.0.3</option>
-            <option value="111">11.1</option>
-        </select>
+    <div class="logo">
+        <h1>
+            <img src="images/ios-logo.png" alt="iOS">
+            <span>iOS iCloud Unlock</span>
+        </h1>
     </div>
-    <div class="device">
-        <div class="screen"></div>
+
+    <div class="selector">
+        <form action="">
+            <input type="text" placeholder="Enter your ECID">
+            <input type="text" placeholder="Enter your serial number">
+            <select name="ios" id="ios">
+                <option class="default" value="" disabled selected>Select your iOS version</option>
+                <option value="1033">10.3.3</option>
+                <option value="1100">11.0.0</option>
+                <option value="1103">11.0.3</option>
+                <option value="111">11.1</option>
+            </select>
+            <button type="submit">
+                Submit
+            </button>
+        </form>
     </div>
-</div>
-
-<div class="selector">
-
-</div>
 </body>
 </html>

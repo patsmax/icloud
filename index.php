aa7d529938e75453c848da1c125bf6a3e62c5164
maksimsivanovs <maksims.ivanovs@redboxdigital.com>
2017-11-27 22:03:12 +0200

    Images and tempalte

--- index.php
+++ index.php
@@ -6,9 +6,36 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>iCloud Unlock</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <link rel="stylesheet" href="stylesheets/main.css">
 </head>
 <body>
+<div class="logo">
+    <h1>
+        <img src="images/ios-logo.png" alt="iOS">
+        <span>iOS iCloud Unlock</span>
+    </h1>
+</div>
+<div class="device-background">
+    <div class="device-text">
+        <h4>Enter your details to unlock</h4>
+        <input type="text" placeholder="Enter your ECID">
+        <input type="text" placeholder="Enter your serial number">
+        <select name="ios" id="ios">
+            <option value="1033">10.3.3</option>
+            <option value="1100">11.0.0</option>
+            <option value="1103">11.0.3</option>
+            <option value="111">11.1</option>
+        </select>
+    </div>
+    <div class="device">
+        <div class="screen"></div>
+    </div>
+</div>
 
+<div class="selector">
+
+</div>
 </body>
 </html>
